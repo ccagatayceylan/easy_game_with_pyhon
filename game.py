e4c30550655c83750a828641df1ca76b301decb9
ccagatayceylan <119412495+ccagatayceylan@users.noreply.github.com>
2023-01-20 16:05:44 +0300

    Add files via upload

---
+++ game.py
@@ -0,0 +1,35 @@
+name = input("Enter Name: ")
+print("Welcome " + name + " time to play game")
+
+secret_word = "Cyberman"
+
+guess_string = ""
+
+lives = 10
+
+while lives > 0:
+
+	character_left = 0
+
+	for character in secret_word:
+		if character in guess_string:
+			print(character)
+		else:
+			print("-")
+			character_left += 1
+
+	if character_left==0:
+		print("You won!")
+		break
+
+	guess = input("Guess a one letter: ")
+	guess_string += guess
+
+	if guess not in secret_word:
+		lives -= 1
+		print("Wrong guess!")
+		print(f"You have {lives} left")
+
+		if lives ==0:
+			print("You died!")
+
